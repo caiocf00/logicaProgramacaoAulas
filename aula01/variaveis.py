8c17226927228f46cc755eb5a7aa6b65e8108f1a
caiocf00 <caio.jesus@aluno.senai.br>
2025-08-07 12:09:19 -0300

    aula de variaveis

---
+++ aula01/variaveis.py
@@ -0,0 +1,112 @@
+print("ola mundo")
+print("ola mundo")
+print("ola mundo")
+print("ola mundo")
+print("ola mundo")
+print("ola mundo")
+print("ola mundo")
+'''
+comentario'''
+'''oi
+'''
+print(30*"-")
+nome = "cabeça"
+print("Nome:" ,nome)
+
+''' crie um sistema que mostre o nome e idade e data de nascimento'''
+nome = "Caio"
+idade = 25
+data_nascimento = "01/01/2000"
+print("Nome:", nome)
+print("Idade:", idade)
+print("Data de Nascimento:", data_nascimento)      
+print("Ola me chamo", nome, "\ntenho", idade, "anos e nasci no dia", data_nascimento)
+
+
+# tipos de variaveis
+
+nome = "Caio"
+idade = 25 
+altura = 1.75
+ativo = True
+print("o tipo da variavel nome e:",type (nome))
+print("o tipo da variavel nome e:",type (idade))
+print("o tipo da variavel nome e:",type (altura))
+print("o tipo da variavel nome e:",type (ativo))
+
+#operadores
+num1 = 10
+num2  = 5
+
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+expo = num1 ** num2
+divisao = num1 / num2
+print("Resultado da soma", num1, "+", num2, "=", soma)
+print("Resultado da soma", num1, "/", num2, "=", divisao)
+print("Resultado da soma", num1, "*", num2, "=", multiplicacao)
+print("Resultado da soma", num1, "**", num2, "=", expo)
+print("Resultado da soma", num1, "-", num2, "=", subtracao)
+
+# Operadores de comparação
+maior = num1 > num2
+menor = num1 < num2
+igual = num1 == num2
+num1 >= num2
+num1 <= num2
+num1 != num2
+
+ano = 2022
+print("O ano atual é:", ano)
+ano += 1  
+print("O ano acrescido de +1 e :", ano)
+ano -= 1
+print("O ano diminuido de -1 e :", ano)
+
+# Operadores lógicos
+# And, or, not
+
+print(30*"-","Entrada de dados",30*"-")
+
+nome = input("Digite seu nome: ")
+print("Seja bem-vindo ao sitema python", nome)
+
+print(30*"-","Calculadora",30*"-")
+num1 = int(input("Digite o primeiro número: "))
+
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+expo = num1 ** num2
+divisao = num1 / num2 
+
+print("Resultado da soma", num1, "+", num2, "=", soma)
+print("Resultado da soma", num1, "/", num2, "=", divisao)
+print("Resultado da soma", num1, "*", num2, "=", multiplicacao)
+print("Resultado da soma", num1, "**", num2, "=", expo)
+print("Resultado da soma", num1, "-", num2, "=", subtracao)
+
+# float = numeros com virgulas = 10.5
+# int = numeros inteiros = 10
+
+print(30*"-","Convertendo tipos de dados",30*"-")
+
+ano_nascimentos = input("Digite o ano do seu nascimento: ")
+print(type(ano_nascimentos)) 
+ # Verificando o tipo da variável
+ # Verificando o tipo da variável
+# convertendo para inteiro
+
+ano_nascimentos = int(ano_nascimentos)
+print(type(ano_nascimentos)) 
+
+n1 = 10
+n2 = 5
+print("A soma e :" , n1 + n2, type(n1), float(n2))
+
+saudacao = input("Digite seu nome: ")
+cpf = input("Digite seu CPF: ")
