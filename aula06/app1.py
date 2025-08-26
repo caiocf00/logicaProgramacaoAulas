a2a4c50e73ad0165e4ce21a4749ca30cd904e94e
caiocf00 <caio.jesus@aluno.senai.br>
2025-08-26 11:48:57 -0300

    json

---
+++ aula06/app1.py
@@ -0,0 +1,17 @@
+# dicionario
+
+usuario = {
+    'nome': 'caio',
+    'idade': 18 
+}
+
+print(type(usuario))
+print(usuario)
+
+#print(f'Nome: {usuario['nome']}')
+#print(f'Nome: {usuario['idade']}')
+
+
+usuarios = ['gomes', 'nunes', 'cris']
+print(usuarios)
+print(f'Nome: {usuarios['']}')
